364c83245a848ad62e262ae209f147501fc03c1e
dianiivanov <dian.ivanov@lights.digital>
2020-12-30 19:44:05 +0200

    Initialize project

---
+++ RemixContestOrganizer/src/main/java/com/organizer/contest/remix/RemixContestOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.organizer.contest.remix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemixContestOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemixContestOrganizerApplication.class, args);
+	}
+
+}
---
+++ RemixContestOrganizer/src/test/java/com/organizer/contest/remix/RemixContestOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizer.contest.remix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemixContestOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
