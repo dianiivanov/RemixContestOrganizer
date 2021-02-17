6eb1426fb7ff8994f51c6212fa74c3a86327b6dc
dianiivanov <dian.ivanov@lights.digital>
2021-02-18 00:21:58 +0200

    Remove unnecessary dependencies

--- RemixContestOrganizer/src/main/java/com/organizer/contest/remix/config/AppConfig.java
+++ RemixContestOrganizer/src/main/java/com/organizer/contest/remix/config/AppConfig.java
@@ -1,15 +1,12 @@
 package com.organizer.contest.remix.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.content.fs.config.EnableFilesystemStores;
-import org.springframework.content.fs.io.FileSystemResourceLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 
 @Configuration
-@EnableFilesystemStores
 public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
